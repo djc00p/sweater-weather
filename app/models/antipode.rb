a7ce834b91ef3d7972baf8de7edadee29d9c4f24
Deonte Cooper <deonte.cooper12@gmail.com>
2019-06-03 10:10:53 -0600

    Added service

--- app/models/antipode.rb
+++ app/models/antipode.rb
@@ -6,15 +6,21 @@ class Antipode
 
 
   def antipode_city
-    conn = Faraday.new('http://amypode.herokuapp.com/api/v1/antipodes') do |f|
-      f.headers[:api_key] = ENV['ANTIPODE_KEY']
-      f.params[:loc] = @location
-      f.adapter Faraday.default_adapter
-    end
-
-    response = conn.get
-
-    data = JSON.parse(response.body, symbolize_names: true)
     binding.pry
+    google_service(@location)
+  end
+
+  private
+
+  def anitpode_service
+    @_antipode_service ||= AntipodeService.new(@location).antipode_location
+  end
+
+  def google_service(location)
+    @_google_service ||= GoogleGeocodingService.new(location)
+  end
+
+  def locate_antipode
+    google_service(@location)
   end
 end
---
+++ app/services/antipode_service.rb
@@ -0,0 +1,23 @@
+class AntipodeService
+  def initialize(location)
+    @location = location
+  end
+
+  def antipode_location
+    get_json
+  end
+
+  def conn
+    Faraday.new('http://amypode.herokuapp.com/api/v1/antipodes') do |f|
+      f.headers[:api_key] = ENV['ANTIPODE_KEY']
+      f.params[:loc] = @location
+      f.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_json
+    response = conn.get
+
+    JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+  end
+end
